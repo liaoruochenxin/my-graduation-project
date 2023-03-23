b92ff145067304d61f36e84862292609e203b618
liaoruochenxin <2196716491@qq.com>
2023-03-23 19:43:25 +0800

    2023/3/23有问题的一次提交

--- more data/test.py
+++ more data/test.py
@@ -3,6 +3,8 @@ import time
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import csv
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class Spider:
@@ -35,20 +37,48 @@ class Spider:
             self.driver.execute_script(
                 'window.scrollTo(0, document.body.scrollHeight)'  # 0表示x轴滚动距离，后面的表示y轴滚动到底
             )
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_all_elements_located((By.XPATH, "//div[@class='comment']")))  # 等待指定元素出现
             comments = self.driver.find_elements(By.XPATH, '//div[@id="comments"]/div[@class="comment-item "]/div[@class="comment"]/p/span[@class="short"]')
-            grades = self.driver.find_elements(By.XPATH, '')
+            gelements = self.driver.find_elements(By.XPATH, '//div[@id="comments"]/div[@class="comment-item "]/div[@class="comment"]/h3/span[@class="comment-info"]/span[2]')
+            dates = self.driver.find_elements(By.XPATH, '//div[@id="comments"]/div[@class="comment-item "]/div[@class="comment"]/h3/span[@class="comment-info"]/span[3]')
+            areas = self.driver.find_elements(By.XPATH, '//div[@id="comments"]/div[@class="comment-item "]/div[@class="comment"]/h3/span[@class="comment-info"]/span[4]')
             time.sleep(1)
+            for date in dates:
+                dic = {}
+                dic["times"] = date.text
+                self.lst.append(dic)
+            for area in areas:
+                dic = {}
+                dic["areas"] = area.text
+                self.lst.append(dic)
             for comment in comments:
                 dic = {}
                 dic["comments"] = comment.text
                 self.lst.append(dic)
+            for gelement in gelements:
+                dic = {}
+                dic["grades"] = gelement.get_attribute("class")
+                if dic["grades"] == "allstar50 rating":
+                    dic["grades"] = 5
+                elif dic["grades"] == "allstar40 rating":
+                    dic["grades"] = 4
+                elif dic["grades"] == "allstar30 rating":
+                    dic["grades"] = 3
+                elif dic["grades"] == "allstar20 rating":
+                    dic["grades"] = 2
+                elif dic["grades"] == "allstar10 rating":
+                    dic["grades"] = 1
+                self.lst.append(dic)
             self.driver.find_element(By.XPATH, '//div[@id="paginator"]/a[3]').click()
         print(self.lst)
-        # print(self.lst)
         # with open(f'{mname}.csv', 'w', encoding='utf-8', newline='') as f:
-        #     writer = csv.DictWriter(f, fieldnames=['comments', 'grade'])
+        #     writer = csv.DictWriter(f, fieldnames=['comments', 'grades', 'times', 'areas'])
         #     writer.writeheader()
         #     writer.writerows(self.lst)
+        with open(f'千寻小姐.csv', 'w', encoding='GB18030', newline='') as f:
+            writer = csv.DictWriter(f, fieldnames=['comments', 'grades', 'times', 'areas'])
+            writer.writeheader()
+            writer.writerows(self.lst)
 
 spider = Spider()
 spider.getdata()
